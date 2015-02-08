bce2081645a9318ec53758bda8e31d6652684f2b
Giacomo Tagliabue <giacomo.tag@gmail.com>
2015-02-08 12:11:31 +0100

    Refactor functions. They were using tuples before, so they were not curryable

--- src/List.Examples.fs
+++ src/List.Examples.fs
@@ -4,20 +4,20 @@ module Examples =
   
   open whyfp.Lists
 
-  let list1 = Cons(1, Cons(2, Cons(3, Cons(4, Nil)))) // [1, 2, 3, 4]
+  let list1 = Cons 1 (Cons 2 (Cons 3 (Cons 4 Nil))) // [1, 2, 3, 4]
 
   let sum1 = sum list1 // 10
 
   let prod1 = prod list1 // 24
 
-  let list2 = Cons(true, Cons(true, Cons(false, Nil))) // [true, true, false]
+  let list2 = Cons true (Cons true (Cons false Nil)) // [true, true, false]
 
   let true1 = anyTrue list2 // true
 
   let false1 = allTrue list2 // false
 
-  let list3 = Cons(5, Cons(6, Cons(7, Cons(8, Nil)))) // [5, 6, 7, 8]
+  let list3 = Cons 5 (Cons 6 (Cons 7 (Cons 8 Nil))) // [5, 6, 7, 8]
 
-  let list4 = append(list1, list3) // [1, 2, 3, 4, 5, 6, 7, 8]
+  let list4 = append list1 list3 // [1, 2, 3, 4, 5, 6, 7, 8]
 
   let lit4Lenght = length list4 // 8
--- src/List.fs
+++ src/List.fs
@@ -1,39 +1,29 @@
 ﻿namespace whyfp
 
 module Lists = 
-  /// List type built out of Nil and Cons primitives
-  /// e.g: Cons(1, Cons(2, Cons(3, Nil)))
-  type List<'T> = 
-    | Nil
-    | Cons of ('T * List<'T>)
 
-    override this.ToString() = 
-      "[" + this.toStringInt() + "]"
-  
-    member private this.toStringInt() = 
-      match this with
-      | Nil -> ""
-      | Cons(a, Cons(b, c)) -> a.ToString() + ", " + Cons(b,c).toStringInt()
-      | Cons(a, Nil) -> a.ToString()
+  let Nil = []
 
-  let rec foldr (l , f, zero) =
+  let Cons a b = a :: b
+
+  let rec foldr f zero l =
     match l with
-    | Nil -> zero
-    | Cons(a, b) -> f (a, foldr (b, f, zero))
+    | [] -> zero
+    | a :: b -> f a (foldr f zero b)
 
-  let plus (a, b) = a + b
-  let sum l = foldr (l, plus, 0)
+  let plus a b = a + b
+  let sum l = foldr (plus) 0 l
 
-  let times (a, b) = a * b
-  let prod l = foldr (l, times, 1)
+  let times a b = a * b
+  let prod l = foldr times 1 l
 
-  let or_ (a, b) = a || b
-  let anyTrue l = foldr (l, or_, false)
+  let or_ a b = a || b
+  let anyTrue l = foldr or_ false l
 
-  let and_ (a, b) = a && b
-  let allTrue l = foldr (l, and_, true)
+  let and_ a b = a && b
+  let allTrue l = foldr and_ true l
 
-  let append(a, b) = foldr(a, Cons, b)
+  let append l1 l2 = foldr Cons l2 l1
 
-  let count(a, n) = n + 1
-  let length a = foldr(a, count, 0)
+  let count a b = a + 1
+  let length l = foldr count 0 l
